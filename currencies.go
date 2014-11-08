eb3353800396b024a5ea5cc2f001194c9fe40bf2
Sevki Hasirci <s@sevki.org>
2014-11-08 16:44:08 +0200

    API changes for making this a pure HTTP client

--- currencies.go
+++ currencies.go
@@ -1,44 +1,29 @@
 // Package currencies provides a simple interface to the http://openexchangerates.org api
-// It can be used to keep a local copy of the latest state of the api.
-//
-// Usage:
-// First, you need to import the package as usual and then set the AppId by calling the
-// AppId function:
-//				AppId("s0m34pp1d")
-// Then you can spawn the Updater as a goroutine that will poll the api every
-// "interval" of time and update the local state
 package currencies
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"errors"
+	"io"
 	"log"
 	"net/http"
-	"regexp"
-
-	"github.com/hailocab/i18n-go/money"
+	"time"
 )
 
 const (
-	apiBase    = "http://openexchangerates.org/api"
+	apiBase    = "https://openexchangerates.org/api"
 	latest     = apiBase + "/latest.json?app_id="
 	currencies = apiBase + "/currencies?app_id="
 )
 
-var (
-	appID   string = ""
-	client  *http.Client
-	current *ExchangeRate
-)
-
-func init() {
-	client = &http.Client{}
+type OpenExchangeClient struct {
+	AppID  string
+	Client *http.Client
 }
 
 // ExchangeRate is used to marshal the current rates from the json api
 // at https://openexchangerates.org/documentation
-type ExchangeRate struct {
+type exchangeRate struct {
 	Disclaimer string             `json:"disclaimer"`
 	License    string             `json:"license"`
 	Timestamp  int64              `json:"timestamp"`
@@ -49,120 +34,42 @@ type ExchangeRate struct {
 // Rate is used to wrap a single exchange rate value
 type Rate struct {
 	Name  string
-	Value float64
+	Value int64
+	When  time.Time
 }
 
-// AppID sets the local app_id key used to access the api. It validates
-// the api key via regexp, so no test of functioning is done and a wrong api key
-// can lead to runtime errors
-func AppID(id string) (err error) {
-	pattern := "[a-zA-Z0-9]{32}"
-	ok, err := regexp.MatchString(pattern, id)
-	if err != nil {
-		return fmt.Errorf("Error validating regexp: %s", err)
+// Get Latest returns the latest rates.
+func (c *OpenExchangeClient) GetLatest() (rates []Rate, err error) {
+	if c.AppID == "" {
+		return nil, errors.New("No app Id")
 	}
-	if !ok {
-		return fmt.Errorf("Invalid app_id provided")
+	if c.Client == nil {
+		c.Client = &http.Client{}
 	}
-	appID = id
-	return nil
-}
 
-func doRequest(url string) (data []byte, err error) {
-	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Add("Content-Type", "application/json")
+	resp, err := c.Client.Get(latest + c.AppID)
+	log.Println(latest + c.AppID)
 	if err != nil {
 		return nil, err
+	} else if resp.StatusCode != 200 {
+		panic(resp.Status)
 	}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
+
 	defer resp.Body.Close()
-	data, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
+
+	ex := new(exchangeRate)
+	dec := json.NewDecoder(resp.Body)
+
+	if err := dec.Decode(&ex); err == io.EOF {
+
+	} else if err != nil {
+		panic(err)
 	}
+	var rs []Rate
+	t := time.Unix(ex.Timestamp, 0)
+	for name, cur := range ex.Rates {
+		rs = append(rs, Rate{Name: name, Value: int64(cur * 100), When: t})
+	}
+	rates = rs
 	return
-}
-
-func setNewRates(ex *ExchangeRate) {
-	current = ex
-}
-
-func validCurrency(cur string) bool {
-	_, valid := current.Rates[cur]
-	return valid
-}
-
-func getLatest() (ex *ExchangeRate, err error) {
-	if appID == "" {
-		panic("Required AppID() not configured!")
-	}
-	ex = new(ExchangeRate)
-	log.Println("Updating exchange rates")
-	url := latest + appID
-	data, err := doRequest(url)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(data, &ex)
-	if err != nil {
-		return nil, err
-	}
-	return
-}
-
-// Update is updates the current vision of the exchange rates
-// of the library from the net
-func Update() (err error) {
-	log.Println("Fetching new currencies")
-	ex, err := getLatest()
-	if err != nil {
-		log.Printf("Could not update rates: %s", err)
-		return err
-	}
-	setNewRates(ex)
-	return nil
-}
-
-// GetRate queries the current state of the view of the exchange rates
-// and returns the rate for the given currency if available or an error if the
-// selected currency does not exist
-func GetRate(cur string) (rate float64, err error) {
-	rate, ok := current.Rates[cur]
-	if !ok {
-		return rate, fmt.Errorf("Currency %s not available", cur)
-	}
-	return rate, nil
-}
-
-// GetRates returns a buffered channel that holds at most the current
-// number of exchange rates. A slow reading client will receive all the rates
-// as they were at the moment the function was called (a copy of the struct is passed to
-// the inner goroutine)
-func GetRates() <-chan (Rate) {
-	rates := make(chan (Rate), len(current.Rates))
-	go func(cur ExchangeRate, c chan (Rate)) {
-		defer close(c)
-		for k, v := range cur.Rates {
-			c <- Rate{k, v}
-		}
-	}(*current, rates)
-	return rates
-}
-
-// Convert converts from one currency to another, returning a converted value or an
-// error if either curency does not exist
-func Convert(amount *money.Money, to string) (converted *money.Money, err error) {
-	if !validCurrency(amount.C) {
-		return nil, fmt.Errorf("Currency %s does not exist or is not available", amount.C)
-	}
-	if !validCurrency(to) {
-		return nil, fmt.Errorf("Currency %s does not exist or is not available", to)
-	}
-	fromRate, _ := current.Rates[amount.C]
-	toRate, _ := current.Rates[to]
-	fromToRate := toRate * (1 / fromRate)
-	return money.New(amount.Mulf(fromToRate).Value(), to), nil
 }
--- currencies_test.go
+++ currencies_test.go
@@ -1,98 +1,43 @@
-package currencies
+package currencies_test
 
 import (
-	"log"
-	"testing"
-	"time"
-
-	"github.com/hailocab/i18n-go/money"
+	"appengine"
+	"appengine/urlfetch"
 )
 
-var testExchangeRates = &ExchangeRate{
-	Disclaimer: "Disclaimer",
-	License:    "License",
-	Timestamp:  time.Now().Unix(),
-	Base:       "USD",
-	Rates: map[string]float64{
-		"EUR": 0.7919,
-		"GBP": 0.6166,
-		"USD": 1.0,
-	},
-}
-
-func TestAppIdWrongKey(t *testing.T) {
-	err := AppID("someappid")
-	if err == nil {
-		t.Fatalf("Regexp validation failed")
+// Create an OpenExchange Client
+func ExampleOpenExchangeClient() {
+	oe := OpenExchangeClient{
+		AppID: "{APP_ID}",
 	}
 }
 
-func TestAppIdEmptyKey(t *testing.T) {
-	err := AppID("")
-	if err == nil {
-		t.Fatalf("Accepted empty app_id")
+// If you are using GAE or have an existing http.Client
+// You can use it's context to fetch the rates like so
+func ExampleOpenExchangeClient_gae() {
+	c := appengine.NewContext(r)
+	client := urlfetch.Client(c)
+	oe := OpenExchangeClient{
+		AppID:  "{APP_ID}",
+		Client: client,
 	}
 }
 
-func TestGetRate(t *testing.T) {
-	current = testExchangeRates
-	rate, err := GetRate("EUR")
-	if err != nil || rate != 0.7919 {
-		t.Fatalf("Error retrieving expected value")
+// Get the latest rates
+func ExampleOpenExchangeClient_GetLatest() {
+	oe := OpenExchangeClient{
+		AppID: "{APP_ID}",
 	}
-	rate, err = GetRate("NONEXISTENT")
-	if err == nil {
-		t.Fatalf("Allowed fetch of non existing currency")
-	}
+	rates, err := oe.GetLatest()
 }
 
-func TestGetRates(t *testing.T) {
-	for v := range GetRates() {
-		_, ok := current.Rates[v.Name]
-		if !ok {
-			log.Fatalf("Did not retrieve rates for all currencies")
-		}
+// Get the latest rates
+func ExampleOpenExchangeClient_GetLatest_gae() {
+	c := appengine.NewContext(r)
+	client := urlfetch.Client(c)
+	oe := OpenExchangeClient{
+		AppID:  "{APP_ID}",
+		Client: client,
 	}
-}
-
-func TestConvertUSDToUSD(t *testing.T) {
-	converted, _ := Convert(money.New(100, "USD"), "USD")
-	if converted.Get() != 1.0 || converted.C != "USD" {
-		t.Fatalf("Converting USD to USD failed: %f", converted.Get())
-	}
-}
-
-func TestConvertWrongValues(t *testing.T) {
-	c, err := Convert(money.New(100, "USD"), "")
-	if c != nil || err == nil {
-		t.Fatalf("Converting with empty TO succeded")
-	}
-}
-
-func TestConvertEURToUSD(t *testing.T) {
-	converted, _ := Convert(money.New(100, "EUR"), "USD")
-	if converted.Get() != 1.26 || converted.C != "USD" {
-		t.Fatalf("Converting 1 EUR to USD: %.32f", converted)
-	}
-}
-
-func TestConvertGBPToUSD(t *testing.T) {
-	converted, _ := Convert(money.New(100, "GBP"), "USD")
-	if converted.Get() != 1.62 || converted.C != "USD" {
-		t.Fatalf("Converting 1 GBP to USD: %f", converted)
-	}
-}
-
-func TestConvertUSDInEUR(t *testing.T) {
-	converted, _ := Convert(money.New(100, "USD"), "EUR")
-	if converted.Get() != 0.79 || converted.C != "EUR" {
-		t.Fatalf("Converting 1 USD to EUR: %f", converted)
-	}
-}
-
-func TestConvertGBPInEUR(t *testing.T) {
-	converted, _ := Convert(money.New(100, "GBP"), "EUR")
-	if converted.Get() != 1.28 || converted.C != "EUR" {
-		t.Fatalf("Converting 1 GBP to EUR: %f", converted)
-	}
+	rates, err := oe.GetLatest()
 }
